db3b119209fc7e5e262927105ba2efb9f5610ced
vickiychung <vickiychung@gmail.com>
2021-04-18 21:40:34 -0700

    Set up

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const passport = require('passport');
+// const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!!"));
+
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
