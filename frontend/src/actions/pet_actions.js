06957b53ea280b6b38aff45cfb79b65f6a1bd5ed
Ali Ibsin <ibsin.a1996@gmail.com>
2021-04-19 15:15:45 -0700

    add pet util and actions

--- frontend/src/actions/pet_actions.js
+++ frontend/src/actions/pet_actions.js
@@ -1,17 +1,48 @@
-import axios from 'axios';
+import * as PetApiUtil from '../util/pet_api_util';
 
-export const getPets = () => {
-  return axios.Cancel.get('/api/pets')
-};
+export const RECEIVE_PETS = "RECEIVE_PETS";
+export const RECEIVE_USER_PETS = "RECEIVE_USER_PETS";
+export const RECEIVE_PET = "RECEIVE_PET";
+export const RECEIVE_PET_ERRORS = "RECEIVE_PET_ERRORS";
 
-export const userPets = id => {
-  return axios.get(`/api/pets/user/${id}`)
-}
+export const receivePets = pets => ({
+  type: RECEIVE_PETS,
+  pets
+});
 
-export const getPet = id => {
-  return axios.get(`/api/pets/${id}`)
-}
+export const receiveUserPets = pets => ({
+  type: RECEIVE_USER_PETS,
+  pets
+});
 
-export const registerPet = data => {
-  return axios.post(`/api/pets/register`, data)
-}
+export const receivePet = pet => ({
+  type: RECEIVE_PET,
+  pet
+});
+
+export const receivePetErrors = errors => ({
+  type: RECEIVE_PET_ERRORS,
+  errors
+})
+
+export const fetchPets = () => dispatch => (
+  PetApiUtil.fetchPets()
+  .then(pets => dispatch(receivePets(pets)))
+);
+
+export const fetchUserPets = id => dispatch => (
+  PetApiUtil.fetchUserPets(id)
+  .then(pets => dispatch(receiveUserPets(pets)))
+);
+
+export const fetchPet = petId => dispatch => (
+  PetApiUtil.fetchPet(petId)
+  .then(pet => dispatch(receivePet(pet)))
+);
+
+export const registerPet = data => dispatch => (
+  PetApiUtil.registerPet(data)
+    .then(pet => dispatch(receivePet(pet)),
+      err => (dispatch(receivePetErrors(err.responseJSON)))
+    )
+);
