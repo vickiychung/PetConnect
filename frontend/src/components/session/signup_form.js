b194ef8ed5d5eee9dfea26ea6e7dd275ed44dd76
Taylor Musolf <tcmusolf@gmail.com>
2021-04-19 13:18:28 -0700

    Set up frontend session reducers, components and actions

---
+++ frontend/src/components/session/signup_form.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      zipcode: '',
+      password: '',
+      password2: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.resetSessionErrors();
+  }
+
+  
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let user = {
+      email: this.state.email,
+      handle: this.state.handle,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.props.signup(user, this.props.history); 
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {Object.keys(this.state.errors).map((error, i) => (
+          <li key={`error-${i}`}>
+            {this.state.errors[error]}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="signup-form-container">
+        <form onSubmit={this.handleSubmit}>
+          <div className="signup-form">
+            <br/>
+              <input type="text"
+                  value={this.state.username}
+                  onChange={this.update('username')}
+                  placeholder="Username"
+                />
+            <br/>
+              <input type="text"
+                value={this.state.email}
+                onChange={this.update('email')}
+                placeholder="Email"
+              />
+            <br/>
+            <input type="number"
+                value={this.state.zipcode}
+                onChange={this.update('zipcode')}
+                placeholder="Zip Code"
+              />
+            <br/>
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                placeholder="Password"
+              />
+            <br/>
+              <input type="password"
+                value={this.state.password2}
+                onChange={this.update('password2')}
+                placeholder="Confirm Password"
+              />
+            <br/>
+            <input type="submit" value="Submit" />
+            {this.renderErrors()}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SignupForm);
