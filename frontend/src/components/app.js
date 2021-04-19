3de9ba253257f7a1d1948fa9ffb89c95d0c38119
Ali Ibsin <ibsin.a1996@gmail.com>
2021-04-19 11:12:39 -0700

    set up frontend

---
+++ frontend/src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+
+const App = () => (
+  <div>
+    <p>Hello pets</p>
+    <Switch>
+      
+      
+    </Switch>
+  </div>
+);
+
+export default App;
