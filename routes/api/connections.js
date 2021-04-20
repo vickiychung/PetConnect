688874b89327cbe97aed539107a5fb537e829f7b
vickiychung <vickiychung@gmail.com>
2021-04-19 22:04:14 -0700

    Add connections POST

--- routes/api/connections.js
+++ routes/api/connections.js
@@ -4,3 +4,17 @@ const mongoose = require('mongoose');
 
 const Connection = require('../../models/Connection');
 
+router.post('/',
+  (req, res) => {
+    const newConnection = new Connection({
+      pet1: req.body.pet1,
+      pet2: req.body.pet2
+    });
+
+    newConnection.save()
+      .then(connection => res.json(connection))
+      .catch(err => res.json(err))
+  }
+);
+
+module.exports = router;
