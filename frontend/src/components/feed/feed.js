6c9f0135acd6665261a9a0697a33418c6ca3a282
jason <jmasters1000@yahoo.com>
2021-04-20 12:08:55 -0700

    Work on pets near you

--- frontend/src/components/feed/feed.js
+++ frontend/src/components/feed/feed.js
@@ -3,6 +3,7 @@ import React from 'react';
 import "./feed.css"
 import NavbarContainer from '../navbar/navbar_container';
 import ConnectPotContainer from '../connections/connect_pot_container';
+import PetsNearYouContainer from './pets_near_you/pets_near_you_container'
 
 class Feed extends React.Component {
 
@@ -15,6 +16,9 @@ class Feed extends React.Component {
         <div>
           <div>
             <ConnectPotContainer />
+          </div>
+          <div>
+            <PetsNearYouContainer />
           </div>
         </div>
       </div>
---
+++ frontend/src/components/feed/pets_near_you/pets_near_you.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class PetsNearYou extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  // componentDidMount() {
+  //   this.props.fetchPets()
+  // }
+
+  render() {
+    if (this.props.pets === undefined) {
+      return null
+    }
+    let pets = this.props.pets
+
+    let matchZip = (pets) => {
+      let matches = [];
+
+      for (let i = 0; i < pets.length; i++) {
+        let pet = pets[i];
+        // if (this.props.user.zipcode === pet.shelterZip) {
+        //   matches.push(pet.name)
+        // }
+        if (94063 === pet.shelterZip) {
+          matches.push(pet.name)
+        }
+      }
+      return matches
+    }
+
+    return (
+      <div>
+        <div>
+          Pets Near You
+        </div>
+        <div>
+          {matchZip(pets)}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PetsNearYou;
