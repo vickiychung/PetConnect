e848ec8e037b6a5d44943dcf2549423939aef365
vickiychung <vickiychung@gmail.com>
2021-04-19 21:46:59 -0700

    Start pets patch

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -23,37 +23,59 @@ router.get('/user/:user_id', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    Pet.findById(req.params.id)
-        .then(pet => res.json(pet))
-        .catch(err =>
-            res.status(404).json({ nopetfound: 'No pet found with that ID' })
-        );
+  Pet.findById(req.params.id)
+      .then(pet => res.json(pet))
+      .catch(err =>
+          res.status(404).json({ nopetfound: 'No pet found with that ID' })
+      );
 });
 
 router.post('/register',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validatePetInput(req.body);
-  
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
-  
-      const newPet = new Pet({
-        species: req.body.species,
-        breed: req.body.breed,
-        gender: req.body.gender,
-        size: req.body.size,
-        name: req.body.name,
-        personality: req.body.personality,
-        shelter: req.body.shelter,
-        shelterZip: req.body.shelterZip,
-        age: req.body.age,
-        user: req.user.id
-      });
-  
-      newPet.save().then(pet => res.json(pet));
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePetInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
-  );
+
+    const newPet = new Pet({
+      species: req.body.species,
+      breed: req.body.breed,
+      gender: req.body.gender,
+      size: req.body.size,
+      name: req.body.name,
+      personality: req.body.personality,
+      shelter: req.body.shelter,
+      shelterZip: req.body.shelterZip,
+      age: req.body.age,
+      user: req.user.id
+    });
+
+    newPet.save().then(pet => res.json(pet));
+  }
+);
+
+router.patch('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePetInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Pet.findByIdAndUpdate(
+      req.params.id, 
+      {$set: req.body}, 
+      {new: true}, 
+      (err, result) => {
+        if(err) {
+          return res.status(400).json(errors);
+        }
+        res.send("Updated");
+    });
+  }
+);
 
 module.exports = router;
