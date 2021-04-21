20fe40e311e440cff9080120529ae99df0e7b458
vickiychung <vickiychung@gmail.com>
2021-04-20 19:04:09 -0700

    Try again

--- app.js
+++ app.js
@@ -24,15 +24,20 @@ app.get("/", (req, res) => res.send("Hello World!!"));
 //   }));
 // })
 
-
 app.use(passport.initialize());
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/pets", pets);
 app.use("/api/connections", connections);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
