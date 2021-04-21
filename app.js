ac215cda04c23fc9d8caa7ddd3bafe046b5b0792
Ali Ibsin <ibsin.a1996@gmail.com>
2021-04-21 13:55:17 -0700

    new commit

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ require('./config/passport')(passport);
 
 app.use(cors());
 
-app.use(cors());
-
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
