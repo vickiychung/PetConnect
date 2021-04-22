0233b00196a34e0ae75e6b07ae4b29cfc75bc53b
Taylor Musolf <tcmusolf@gmail.com>
2021-04-21 22:02:11 -0700

    Create pet const in feed and update feed to have petId in url

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -14,16 +14,16 @@ import PickPetContainer from './session/pick/pick_pet_container';
 const App = () => (
   <div>
     <Modal />
-    {/* <p>Hello pets</p> */}
 
     <Switch>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <ProtectedRoute exact path="/pick_pet" component={PickPetContainer} />
-      <ProtectedRoute exact path="/feed" component={FeedContainer} />
+      {/* <ProtectedRoute exact path="/feed" component={FeedContainer} /> */}
+      <ProtectedRoute exact path="/feed/:petId" component={FeedContainer} />
     
       <Redirect exact from="/*" to="/login" />
       <Redirect exact from="/login/*" to="/login" />
-      <Redirect exact from="/feed/*" to="/feed" />
+      {/* <Redirect exact from="/feed/*" to="/feed" /> */}
       <Redirect exact from="/pick_pet/*" to="/pick_pet" />
 
     </Switch>
