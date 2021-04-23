a7d19b8c38f122f9f51ed347fb0cde56ec495f44
Taylor Musolf <tcmusolf@gmail.com>
2021-04-23 09:31:54 -0700

    Class name fix

--- frontend/src/components/session/pick/pick_pet_item.js
+++ frontend/src/components/session/pick/pick_pet_item.js
@@ -13,7 +13,7 @@ class PickPetItem extends React.Component {
           <div className="pet-individual" onClick={() => this.props.fetchCurrentPet(this.props.pet._id)}>
             <div>
               {
-                this.props.pet.photoUrl ? <div class="pet-img"><img src={this.props.pet.photoUrl} /></div>
+                this.props.pet.photoUrl ? <div className="pet-img"><img src={this.props.pet.photoUrl} /></div>
                 : <div className="pet-icon"><FontAwesomeIcon icon={faPaw} /></div>
               }
             </div>
