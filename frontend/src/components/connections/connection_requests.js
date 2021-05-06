bc0c8007ab9a744672d8e6237067ac2df2fcf9c3
Ali Ibsin <ibsin.a1996@gmail.com>
2021-05-05 18:07:59 -0700

    add sentRequest state

--- frontend/src/components/connections/connection_requests.js
+++ frontend/src/components/connections/connection_requests.js
@@ -7,42 +7,71 @@ class ConnectionRequests extends React.Component {
 
     this.state = {
       connectionRequests: this.props.connectionRequests,
+      sentRequests: this.props.sentRequests
     }
   }
 
   componentDidMount() {
     this.props.fetchConnectionRequests(this.props.currentPetId);
+    this.props.fetchSentRequests(this.props.currentPetId);
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.connectionRequests !== this.props.connectionRequests) {
       this.setState({connectionRequests: this.props.connectionRequests});
     };
+    if (prevProps.sentRequests !== this.props.sentRequests) {
+      this.setState({sentRequests: this.props.sentRequests});
+    };
   }
 
   render() {
-    
-    if (!this.state.connectionRequests) {
+    console.log(this.props)
+    console.log(this.state)
+    if (!this.state.connectionRequests || !this.state.sentRequests) {
       return null;
     };
+
+    
     
     return (
-      <ul className="request-list">
-        {
-          this.state.connectionRequests.map((request, i) => (
-            <ConnectRequestItem
-              key={request._id}
-              requestId={request._id}
-              currentPetId={this.props.currentPetId}
-              currentPetName={request.friendName}
-              friendId={request.pet}
-              friendName={request.petName}
-              acceptConnectionRequest={this.props.acceptConnectionRequest}
-              fetchPet={this.props.fetchPet}
-            />
-          ))
-        }
-      </ul>
+      <div>
+        <ul className="request-list">
+          {
+            this.state.connectionRequests.map((request) => (
+              <ConnectRequestItem
+                key={request._id}
+                requestId={request._id}
+                currentPetId={this.props.currentPetId}
+                currentPetName={request.friendName}
+                friendId={request.pet}
+                friendName={request.petName}
+                acceptConnectionRequest={this.props.acceptConnectionRequest}
+                fetchPet={this.props.fetchPet}
+              />
+            ))
+          }
+        </ul>
+        <ul className="request-list">
+          {/* test */}
+          {
+            this.state.sentRequests.map((request) => (
+              request.name
+              // <ConnectRequestItem
+              //   key={request._id}
+              //   requestId={request._id}
+              //   currentPetId={this.props.currentPetId}
+              //   currentPetName={request.friendName}
+              //   friendId={request.pet}
+              //   friendName={request.petName}
+              //   acceptConnectionRequest={this.props.acceptConnectionRequest}
+              //   fetchPet={this.props.fetchPet}
+              // />
+            ))
+          }
+        </ul>
+
+      </div>
     );
   }
 }
--- frontend/src/components/connections/connection_requests_container.js
+++ frontend/src/components/connections/connection_requests_container.js
@@ -5,16 +5,19 @@ import ConnectionRequests from './connection_requests'
 import {
   fetchConnectionRequests,
   acceptConnectionRequest,
+  fetchSentRequests
 } from '../../actions/connection_request_actions';
 
 const mapStateToProps = state => ({
-  connectionRequests: state.entities.connectionRequests
+  connectionRequests: state.entities.connectionRequests,
+  sentRequests: state.entities.sentRequests
 })
 
 const mapDispatchToProps = dispatch => ({
   goGetRequestPet: petId => dispatch(goGetRequestPet(petId)),
   fetchPet: petId => dispatch(fetchPet(petId)),
   fetchConnectionRequests: friendId => dispatch(fetchConnectionRequests(friendId)),
+  fetchSentRequests: petId => dispatch(fetchSentRequests(petId)),
   acceptConnectionRequest: data => dispatch(acceptConnectionRequest(data)),
 })
 
